a665a3bb87cdc17eafbe50faea81684ef57bd2d0
Samuel <samuel.louismarie@gmail.com>
2022-05-02 15:36:23 +0200

    creation html.js et changement de port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req,res )=>{
     res.writeHead(200, {'content-type':'text/html'});
-    res.write('<h1> Hello World Samuel </h1>')
+    res.write(' Hello World Samuel')
     res.end();
 }
 );
