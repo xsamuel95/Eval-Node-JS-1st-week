25fde4a1c39d84283bbdf80157b3ba6e7d3a3f65
Samuel <samuel.louismarie@gmail.com>
2022-05-02 15:12:38 +0200

    creation du server qui renvoie Hello World Samuel

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req,res )=>{
+    res.writeHead(200, {'content-type':'text/html'});
+    res.write('<h1> Hello World Samuel </h1>')
+    res.end();
+}
+);
+
+server.listen(3000);
+console.log('serveur ecoute sur le port 3000');
