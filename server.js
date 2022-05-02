1765ef0c9aa90063239b9dbc114ed5c65037044a
Samuel <samuel.louismarie@gmail.com>
2022-05-02 16:09:00 +0200

    Ajout de la condition avec l'url et de l'interne erreur serveur

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const http = require("http");
 
-const server = http.createServer((req,res )=>{
-    res.writeHead(200, {'content-type':'text/html'});
-    res.write('<h1> Hello World Samuel </h1>')
-    res.end();
-}
-);
 
-server.listen(3000);
-console.log('serveur ecoute sur le port 3000');
+const server = http.createServer((req,res )=>{
+    let avant;
+    try {
+      avant = performance.now()
+      if (req.url === "/") {
+        res.writeHead(200, {'content-type':'text/html'});
+        res.write('<h1>Hello World Samuel</h1>');
+        res.end();
+      } else {
+        res.writeHead(404, {'content-type':'text/html'});
+        res.write('<h1>404 Methode non authorise</h1>');
+        res.end()
+      }
+    } catch (err) {
+      res.writeHead(500, {'content-type':'text/html'});
+      res.write('<h1>500 Erreur Interne au Serveur</h1>');
+      res.end()
+    }
+  } );
+
+
+server.listen(5000);
+console.log('serveur ecoute sur le port 5000');
